2cf483d9945acb59bb23a4d3384c598dac8b184a
SirRailOfGun <Mike1011031@gmail.com>
2022-07-16 10:33:46 -0400

    adds camera controls

---
+++ GMTK Game Jam/Assets/scripts/CameraController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public int gameState = 1; // 0 is combat, 1 is inventory, 2 is loot
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(gameState)
+        {
+            case 0:
+                transform.position = new Vector3(0,5,-10);
+                break;
+            case 1:
+                transform.position = new Vector3(0,-5,-10);
+                break;
+        }
+    }
+}
